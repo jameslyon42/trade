3f5fe167c8c2086f869087ccd2a3afbd14082efd
greggel <gelmangreg@gmail.com>
2017-12-13 00:39:00 -0500

    more css crap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,10 +54,11 @@
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-
+                                <div>
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
                                 </a>
+                                </div>
                             </div>
                         </div>
                     </form>
